a4551278dae093619359bb819824ceb81e4466c0
Francis Galiegue <fgaliegue@gmail.com>
2014-12-31 01:58:56 +0100

    Test refactor: Comparator

--- src/test/java/com/github/fge/lambdas/comparators/ThrowingComparatorTest.java
+++ src/test/java/com/github/fge/lambdas/comparators/ThrowingComparatorTest.java
@@ -1,152 +1,60 @@
 package com.github.fge.lambdas.comparators;
 
-import com.github.fge.lambdas.ThrowingInterfaceBaseTest;
-import com.github.fge.lambdas.ThrownByLambdaException;
-import com.github.fge.lambdas.helpers.MyException;
+import com.github.fge.lambdas.ThrowingInterfaceTest;
 import com.github.fge.lambdas.helpers.Type1;
+import org.mockito.Mockito;
 
 import java.util.Comparator;
 import java.util.concurrent.Callable;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-@SuppressWarnings({"ProhibitedExceptionDeclared", "AutoBoxing",
+@SuppressWarnings({"AutoBoxing", "ProhibitedExceptionDeclared",
     "OverlyBroadThrowsClause"})
 public final class ThrowingComparatorTest
-    extends ThrowingInterfaceBaseTest<ThrowingComparator<Type1>, Comparator<Type1>, Integer>
+    extends ThrowingInterfaceTest<ThrowingComparator<Type1>, ThrowingComparator<Type1>, Comparator<Type1>, Integer>
 {
-    private final Type1 arg1 = Type1.mock();
-    private final Type1 arg2 = Type1.mock();
+    private final Type1 o1 = Type1.mock();
+    private final Type1 o2 = Type1.mock();
 
     public ThrowingComparatorTest()
     {
-        super(42, 421);
+        super(SpiedThrowingComparator::newSpy,
+            () -> Mockito.mock(Comparator.class), 42, 24);
     }
 
     @Override
-    protected ThrowingComparator<Type1> getAlternate()
+    protected void setupFull(final ThrowingComparator<Type1> instance)
         throws Throwable
     {
-        final ThrowingComparator<Type1> spy =
-            SpiedThrowingComparator.newSpy();
-
-        when(spy.doCompare(arg1, arg2)).thenReturn(ret2);
-
-        return spy;
+        when(instance.doCompare(o1, o2)).thenReturn(ret1).thenThrow(checked)
+            .thenThrow(unchecked).thenThrow(error);
     }
 
     @Override
-    protected ThrowingComparator<Type1> getTestInstance()
+    protected void setupAlternate(final ThrowingComparator<Type1> instance)
         throws Throwable
     {
-        final ThrowingComparator<Type1> spy
-            = SpiedThrowingComparator.newSpy();
-
-        when(spy.doCompare(arg1, arg2)).thenReturn(ret1)
-            .thenThrow(checked).thenThrow(unchecked).thenThrow(error);
-
-        return spy;
+        when(instance.doCompare(o1, o2)).thenReturn(ret2);
     }
 
     @Override
-    protected Comparator<Type1> getFallback()
+    protected void setupFallback(final Comparator<Type1> instance)
     {
-        @SuppressWarnings("unchecked")
-        final Comparator<Type1> mock = mock(Comparator.class);
-
-        when(mock.compare(arg1, arg2)).thenReturn(ret2);
-
-        return mock;
+        when(instance.compare(o1, o2)).thenReturn(ret2);
     }
 
     @Override
     protected Callable<Integer> asCallable(final Comparator<Type1> instance)
     {
-        return () -> instance.compare(arg1, arg2);
-    }
-
-    @Override
-    public void testUnchained()
-        throws Throwable
-    {
-        final Comparator<Type1> instance = getTestInstance();
-
-        final Callable<Integer> callable = asCallable(instance);
-
-        assertThat(callable.call()).isEqualTo(ret1);
-
-        verifyCheckedRethrow(callable, ThrownByLambdaException.class);
-
-        verifyUncheckedThrow(callable);
-
-        verifyErrorThrow(callable);
-    }
-
-    @Override
-    public void testChainedWithOrThrow()
-        throws Throwable
-    {
-        final Comparator<Type1> instance
-            = getTestInstance().orThrow(MyException.class);
-
-        final Callable<Integer> callable = asCallable(instance);
-
-        assertThat(callable.call()).isEqualTo(ret1);
-
-        verifyCheckedRethrow(callable, MyException.class);
-
-        verifyUncheckedThrow(callable);
-
-        verifyErrorThrow(callable);
-    }
-
-    @Override
-    public void testChainedWithOrTryWith()
-        throws Throwable
-    {
-        final ThrowingComparator<Type1> first = getTestInstance();
-        final ThrowingComparator<Type1> second = getAlternate();
-
-        final Comparator<Type1> instance = first.orTryWith(second);
-
-        final Callable<Integer> callable = asCallable(instance);
-
-        assertThat(callable.call()).isEqualTo(ret1);
-        assertThat(callable.call()).isEqualTo(ret2);
-
-        verifyUncheckedThrow(callable);
-
-        verifyErrorThrow(callable);
-    }
-
-    @Override
-    public void testChainedWithFallbackTo()
-        throws Throwable
-    {
-        final ThrowingComparator<Type1> first = getTestInstance();
-
-        final Comparator<Type1> second = getFallback();
-
-        final Comparator<Type1> instance = first.fallbackTo(second);
-
-        final Callable<Integer> callable = asCallable(instance);
-
-        assertThat(callable.call()).isEqualTo(ret1);
-        assertThat(callable.call()).isEqualTo(ret2);
-
-        verifyUncheckedThrow(callable);
-
-        verifyErrorThrow(callable);
+        return () -> instance.compare(o1, o2);
     }
 
     public void testChainedWithOrReturn()
         throws Throwable
     {
-        final ThrowingComparator<Type1> first = getTestInstance();
-
-        final Comparator<Type1> instance = first.orReturn(ret2);
+        final Comparator<Type1> instance = getFullInstance().orReturn(ret2);
 
         final Callable<Integer> callable = asCallable(instance);
 
